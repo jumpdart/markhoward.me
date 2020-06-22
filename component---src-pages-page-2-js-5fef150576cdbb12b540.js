af61a3c0cee942956c6b87afbd06add2331b144a
jumpdart <markhoward000@gmail.com>
2020-06-22 18:09:42 -0400

    Updates

---
+++ component---src-pages-page-2-js-5fef150576cdbb12b540.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{p5nM:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),c=n("Bl7J"),o=n("vrFN");t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(o.a,{title:"Page two"}),l.a.createElement("h1",null,"Hi from the second page"),l.a.createElement("p",null,"Welcome to page 2"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-5fef150576cdbb12b540.js.map
